53d4f2d17aff46e92ab1e8d291dd09c47b4d241f
Sergio <sergiozavala@csus.edu>
2020-01-24 10:32:43 -0800

    Add files via upload

---
+++ alu_tb.v
@@ -0,0 +1,25 @@
+`include "alu.v"
+
+module alu_tb;
+
+
+alu utt(a, b, function, out, r15);
+
+initial 
+begin 
+
+$monitor()
+
+end 
+
+initial
+begin
+
+//signed addition 
+#10 
+
+//signed subtraction 
+
+//signed multiplication 
+
+//signed division 
